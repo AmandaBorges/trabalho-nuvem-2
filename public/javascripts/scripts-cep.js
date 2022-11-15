41c2cc472e61a588a92c4206327c5426c88fdb73
Amanda Borges <amandasilvaborges2@gmail.com>
2022-11-15 14:18:56 -0300

    Finalizando tela cep a partir do endereco

--- public/javascripts/scripts-cep.js
+++ public/javascripts/scripts-cep.js
@@ -1,4 +1,16 @@
 var btnCep = document.getElementsByClassName("btn-get-cep")[0];
 btnCep.onclick = function() {
-    console.log("teste btn cep");
+    var state = document.getElementsByName("state")[0].value;
+    var city = document.getElementsByName("city")[0].value;
+    var place = document.getElementsByName("place")[0].value;
+
+    axios.get('/api/zip-code-by-address?state='+state+'&city='+city+'&place='+place)
+  .then(function (response) {
+    console.log(response.data);
+    alert(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    alert(JSON.stringify(error.message));
+  });
+    console.log(state, city,place);
 };
